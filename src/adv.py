db09ee3c589613fe39fafa611821edf873c5a8e3
Ramon Audain <raudain123@gmail.com>
2020-03-11 16:10:18 -0700

    Complete Day 1 MVP

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
 from room import Room
+from player import Player
+from os import system
 
 # Declare all the rooms
 
@@ -49,3 +51,67 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+directions = [
+    {
+        "direction": "n",
+        "deadend": "There is no room to the north.",
+        "move_to": f".n_to"
+    },
+    {
+        "direction": "s",
+        "deadend": "There is no room to the south.",
+        "move_to": ".s_to"
+    },
+    {
+        "direction": "e",
+        "deadend": "There is no room to the east.",
+        "move_to": ".e_to"
+    },
+    {
+        "direction": "w",
+        "deadend": "There is no room to the west.",
+        "move_to": ".w_to"
+    }
+]
+
+system("clear")
+
+user = Player(room["outside"])
+instructions = "Enter n (North), s (South), e (East), or w (West) to move. Pressing q will quit the game: \n"
+
+while True:
+    print(f"\nLocation: {user.current_room.name}\n")
+    print(f"{user.current_room.description}\n")
+
+    moveUser = input(instructions)
+
+    # for i in directions:
+    #     for k,v in i.items():
+
+    if moveUser == "q":
+        print("\nGoodbye, adventurer.\n")
+        quit()
+    elif moveUser == "n":
+        if user.current_room.n_to:
+            user.current_room = user.current_room.n_to
+        else:
+            print("There is no room to the north.")
+    elif moveUser == "s":
+        if user.current_room.s_to:
+            user.current_room = user.current_room.s_to
+        else:
+            print("There is no room to the south.")
+    elif moveUser == "e":
+        if user.current_room.e_to:
+            user.current_room = user.current_room.e_to
+        else:
+            print("There is no room to the east.")
+    elif moveUser == "w":
+        if user.current_room.w_to:
+            user.current_room = user.current_room.w_to
+        else:
+            print("There is no room to the west.")
+    else:
+        input(instructions)
+
--- src/room.py
+++ src/room.py
@@ -1,2 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+
+    n_to = None
+    s_to = None
+    e_to = None
+    w_to = None
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
